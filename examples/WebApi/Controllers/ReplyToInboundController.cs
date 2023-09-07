3c1270485012cc652b4c070d55cf541662756d81
Dovchik <ErehonVL@gmail.com>
2023-09-07 13:12:53 +0200

    chore: use file-scope namespace

--- examples/WebApi/Controllers/ReplyToInboundController.cs
+++ examples/WebApi/Controllers/ReplyToInboundController.cs
@@ -4,41 +4,40 @@ using Sinch.SMS.Batches.Send;
 using Sinch.SMS.Hooks;
 using DeliveryReport = Sinch.SMS.DeliveryReport;
 
-namespace WebApiExamples.Controllers
+namespace WebApiExamples.Controllers;
+
+[ApiController]
+[Route("reply")]
+public class ReplyToInboundController : ControllerBase
 {
-    [ApiController]
-    [Route("reply")]
-    public class ReplyToInboundController : ControllerBase
+    private readonly ISinch _sinch;
+    private readonly ILogger _logger;
+
+    public ReplyToInboundController(ISinch sinch, ILogger<ReplyToInboundController> logger)
     {
-        private readonly ISinch _sinch;
-        private readonly ILogger _logger;
+        _sinch = sinch;
+        _logger = logger;
+    }
 
-        public ReplyToInboundController(ISinch sinch, ILogger<ReplyToInboundController> logger)
+    /// <summary>
+    ///     If you want to handle either <see cref="IncomingTextSms"/> or <see cref="IncomingBinarySms"/>
+    ///     use <see cref="IIncomingSms"/> as a parameter and match by type.
+    /// </summary>
+    /// <param name="incomingSms"></param>
+    [HttpPost(Name = "receive")]
+    public async Task Receive([FromBody] IncomingTextSms incomingSms)
+    {
+        _logger.LogInformation("Incoming {text} from {sender} to {recipient}",
+            incomingSms.Body,
+            incomingSms.From,
+            incomingSms.To);
+        var _ = await _sinch.Sms.Batches.Send(new Request
         {
-            _sinch = sinch;
-            _logger = logger;
-        }
-
-        /// <summary>
-        ///     If you want to handle either <see cref="IncomingTextSms"/> or <see cref="IncomingBinarySms"/>
-        ///     use <see cref="IIncomingSms"/> as a parameter and match by type.
-        /// </summary>
-        /// <param name="incomingSms"></param>
-        [HttpPost(Name = "receive")]
-        public async Task Receive([FromBody] IncomingTextSms incomingSms)
-        {
-            _logger.LogInformation("Incoming {text} from {sender} to {recipient}",
-                incomingSms.Body,
-                incomingSms.From,
-                incomingSms.To);
-            var _ = await _sinch.Sms.Batches.Send(new Request
-            {
-                Body = incomingSms.Body,
-                DeliveryReport = DeliveryReport.None,
-                To = new List<string>() { incomingSms.From },
-                From = incomingSms.To
-            });
-            _logger.LogInformation("The reply was sent");
-        }
+            Body = incomingSms.Body,
+            DeliveryReport = DeliveryReport.None,
+            To = new List<string>() { incomingSms.From },
+            From = incomingSms.To
+        });
+        _logger.LogInformation("The reply was sent");
     }
 }
