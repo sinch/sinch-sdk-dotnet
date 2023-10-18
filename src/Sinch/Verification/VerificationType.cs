d6494aef2ec1a5194404247d4abeb6ae0f051826
Dovchik <ErehonVL@gmail.com>
2023-10-18 19:09:09 +0200

    fix: flashCall VerificationType.cs typo

--- src/Sinch/Verification/VerificationType.cs
+++ src/Sinch/Verification/VerificationType.cs
@@ -5,6 +5,6 @@
         public const string Sms = "sms";
         public const string Seamless = "seamless";
         public const string PhoneCall = "callout";
-        public const string FlashCall = "Feamles";
+        public const string FlashCall = "flashCall";
     }
 }
