18e841daf98c70711c1b7ee1239f9612f7f2fef4
Dovchik <ErehonVL@gmail.com>
2023-11-27 13:30:44 +0100

    feat: add types for Get call

---
+++ src/Sinch/Voice/Calls/Call.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Text;
+using System.Text.Json.Serialization;
+using Sinch.Core;
+
+namespace Sinch.Voice.Calls
+{
+    public sealed class Call
+    {
+        /// <summary>
+        ///     Contains the caller information.
+        /// </summary>
+        public string From { get; set; }
+
+        /// <summary>
+        ///     Contains the callee information.
+        /// </summary>
+        public string To { get; set; }
+
+        /// <summary>
+        ///     Must be &#x60;pstn&#x60; for PSTN.
+        /// </summary>
+        public DomainEnum Domain { get; set; }
+
+
+        /// <summary>
+        ///     The unique identifier of the call.
+        /// </summary>
+        public string CallId { get; set; }
+
+        /// <summary>
+        ///     The duration of the call in seconds.
+        /// </summary>
+        public int? Duration { get; set; }
+
+        /// <summary>
+        ///     The status of the call. Either &#x60;ONGOING&#x60; or &#x60;FINAL&#x60;
+        /// </summary>
+        public StatusEnum Status { get; set; }
+
+
+        /// <summary>
+        ///     Contains the result of a call.
+        /// </summary>
+        public ResultEnum Result { get; set; }
+
+
+        /// <summary>
+        ///     Contains the reason why a call ended.
+        /// </summary>
+        public ReasonEnum Reason { get; set; }
+
+
+        /// <summary>
+        ///     The date and time of the call.
+        /// </summary>
+        public DateTime? Timestamp { get; set; }
+
+
+        /// <summary>
+        ///     An object that can be used to pass custom information related to the call.
+        /// </summary>
+        public object Custom { get; set; }
+
+
+        /// <summary>
+        ///     The rate per minute that was charged for the call.
+        /// </summary>
+        public string UserRate { get; set; }
+
+
+        /// <summary>
+        ///     The total amount charged for the call.
+        /// </summary>
+        public string Debit { get; set; }
+
+
+        /// <summary>
+        ///     Returns the string presentation of the object
+        /// </summary>
+        /// <returns>String presentation of the object</returns>
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("class GetCallResponseObj {\n");
+            sb.Append("  From: ").Append(From).Append("\n");
+            sb.Append("  To: ").Append(To).Append("\n");
+            sb.Append("  Domain: ").Append(Domain).Append("\n");
+            sb.Append("  CallId: ").Append(CallId).Append("\n");
+            sb.Append("  Duration: ").Append(Duration).Append("\n");
+            sb.Append("  Status: ").Append(Status).Append("\n");
+            sb.Append("  Result: ").Append(Result).Append("\n");
+            sb.Append("  Reason: ").Append(Reason).Append("\n");
+            sb.Append("  Timestamp: ").Append(Timestamp).Append("\n");
+            sb.Append("  Custom: ").Append(Custom).Append("\n");
+            sb.Append("  UserRate: ").Append(UserRate).Append("\n");
+            sb.Append("  Debit: ").Append(Debit).Append("\n");
+            sb.Append("}\n");
+            return sb.ToString();
+        }
+
+        /// <summary>
+        ///     The status of the call. Either &#x60;ONGOING&#x60; or &#x60;FINAL&#x60;
+        /// </summary>
+        /// <value>The status of the call. Either &#x60;ONGOING&#x60; or &#x60;FINAL&#x60;</value>
+        [JsonConverter(typeof(EnumRecordJsonConverter<StatusEnum>))]
+        public record StatusEnum(string Value) : EnumRecord(Value)
+        {
+            public static readonly StatusEnum ONGOING = new("ONGOING");
+            public static readonly StatusEnum FINAL = new("FINAL");
+        }
+
+        /// <summary>
+        ///     Contains the result of a call.
+        /// </summary>
+        /// <value>Contains the result of a call.</value>
+        [JsonConverter(typeof(EnumRecordJsonConverter<ResultEnum>))]
+        public record ResultEnum(string Value) : EnumRecord(Value)
+        {
+            public static readonly ResultEnum NotAvailable = new("N/A");
+            public static readonly ResultEnum Answered = new("ANSWERED");
+            public static readonly ResultEnum Busy = new("BUSY");
+            public static readonly ResultEnum NoAnswer = new("NOANSWER");
+            public static readonly ResultEnum Failed = new("FAILED");
+        }
+    }
+
+    /// <summary>
+    ///     Must be &#x60;pstn&#x60; for PSTN.
+    /// </summary>
+    /// <value>Must be &#x60;pstn&#x60; for PSTN.</value>
+    [JsonConverter(typeof(EnumRecordJsonConverter<DomainEnum>))]
+    public record DomainEnum(string Value) : EnumRecord(Value)
+    {
+        public static readonly DomainEnum Pstn = new("pstn");
+    }
+
+    /// <summary>
+    ///     Contains the reason why a call ended.
+    /// </summary>
+    /// <value>Contains the reason why a call ended.</value>
+    [JsonConverter(typeof(EnumRecordJsonConverter<ReasonEnum>))]
+    public record ReasonEnum(string Value) : EnumRecord(Value)
+    {
+        public static readonly ReasonEnum NotAvailable = new("N/A");
+        public static readonly ReasonEnum Timeout = new("TIMEOUT");
+        public static readonly ReasonEnum CallerHangUp = new("CALLERHANGUP");
+        public static readonly ReasonEnum CaleeHangUp = new("CALLEEHANGUP");
+        public static readonly ReasonEnum Blocked = new("BLOCKED");
+        public static readonly ReasonEnum NoCreditPartner = new("NOCREDITPARTNER");
+        public static readonly ReasonEnum ManagerHangUp = new("MANAGERHANGUP");
+        public static readonly ReasonEnum Cancel = new("CANCEL");
+        public static readonly ReasonEnum GeneralError = new("GENERALERROR");
+    }
+}
--- src/Sinch/Voice/Calls/Calls.cs
+++ src/Sinch/Voice/Calls/Calls.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Sinch.Voice.Calls
@@ -19,15 +20,33 @@ namespace Sinch.Voice.Calls
         /// <param name="cancellationToken"></param>
         /// <returns>Successful task if response code is 204</returns>
         Task Update(UpdateCallRequest request, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        ///     ou can retrieve information about an ongoing or completed call using a call ID. You can find the call ID of an
+        ///     ongoing call by viewing the response object from a callout request. You can find the call ID of a completed call by
+        ///     looking at your call logs in your Sinch <see href="https://dashboard.sinch.com/voice/logs">Dashboard</see>.
+        ///     <br /><br />
+        ///     Note: You can only use this method for calls that terminate to PSTN or SIP networks from an In-app call.
+        /// </summary>
+        /// <param name="callId"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<Call> Get(string callId, CancellationToken cancellationToken = default);
     }
 
     /// <inheritdoc />
     internal class Calls : ISinchVoiceCalls
     {
-        /// <inheritdoc /> 
+        /// <inheritdoc />
         public Task Update(UpdateCallRequest request, CancellationToken cancellationToken = default)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public Task<Call> Get(string callId, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
         }
     }
 }
