39e9892805fa6a455d6f4644225a25a05e76cd7a
Dovchik <ErehonVL@gmail.com>
2023-11-28 18:41:24 +0100

    tests: add get call test

--- src/Sinch/Voice/Calls/Call.cs
+++ src/Sinch/Voice/Calls/Call.cs
@@ -20,7 +20,7 @@ namespace Sinch.Voice.Calls
         /// <summary>
         ///     Must be &#x60;pstn&#x60; for PSTN.
         /// </summary>
-        public DomainEnum Domain { get; set; }
+        public CallDomain Domain { get; set; }
 
 
         /// <summary>
@@ -36,19 +36,19 @@ namespace Sinch.Voice.Calls
         /// <summary>
         ///     The status of the call. Either &#x60;ONGOING&#x60; or &#x60;FINAL&#x60;
         /// </summary>
-        public StatusEnum Status { get; set; }
+        public CallStatus Status { get; set; }
 
 
         /// <summary>
         ///     Contains the result of a call.
         /// </summary>
-        public ResultEnum Result { get; set; }
+        public CallResult Result { get; set; }
 
 
         /// <summary>
         ///     Contains the reason why a call ended.
         /// </summary>
-        public ReasonEnum Reason { get; set; }
+        public CallResultReason Reason { get; set; }
 
 
         /// <summary>
@@ -74,6 +74,8 @@ namespace Sinch.Voice.Calls
         /// </summary>
         public string Debit { get; set; }
 
+        public object Should { get; set; }
+
 
         /// <summary>
         ///     Returns the string presentation of the object
@@ -98,58 +100,58 @@ namespace Sinch.Voice.Calls
             sb.Append("}\n");
             return sb.ToString();
         }
+    }
 
-        /// <summary>
-        ///     The status of the call. Either &#x60;ONGOING&#x60; or &#x60;FINAL&#x60;
-        /// </summary>
-        /// <value>The status of the call. Either &#x60;ONGOING&#x60; or &#x60;FINAL&#x60;</value>
-        [JsonConverter(typeof(EnumRecordJsonConverter<StatusEnum>))]
-        public record StatusEnum(string Value) : EnumRecord(Value)
-        {
-            public static readonly StatusEnum ONGOING = new("ONGOING");
-            public static readonly StatusEnum FINAL = new("FINAL");
-        }
+    /// <summary>
+    ///     The status of the call. Either &#x60;ONGOING&#x60; or &#x60;FINAL&#x60;
+    /// </summary>
+    /// <value>The status of the call. Either &#x60;ONGOING&#x60; or &#x60;FINAL&#x60;</value>
+    [JsonConverter(typeof(EnumRecordJsonConverter<CallStatus>))]
+    public record CallStatus(string Value) : EnumRecord(Value)
+    {
+        public static readonly CallStatus ONGOING = new("ONGOING");
+        public static readonly CallStatus FINAL = new("FINAL");
+    }
 
-        /// <summary>
-        ///     Contains the result of a call.
-        /// </summary>
-        /// <value>Contains the result of a call.</value>
-        [JsonConverter(typeof(EnumRecordJsonConverter<ResultEnum>))]
-        public record ResultEnum(string Value) : EnumRecord(Value)
-        {
-            public static readonly ResultEnum NotAvailable = new("N/A");
-            public static readonly ResultEnum Answered = new("ANSWERED");
-            public static readonly ResultEnum Busy = new("BUSY");
-            public static readonly ResultEnum NoAnswer = new("NOANSWER");
-            public static readonly ResultEnum Failed = new("FAILED");
-        }
+    /// <summary>
+    ///     Contains the result of a call.
+    /// </summary>
+    /// <value>Contains the result of a call.</value>
+    [JsonConverter(typeof(EnumRecordJsonConverter<CallResult>))]
+    public record CallResult(string Value) : EnumRecord(Value)
+    {
+        public static readonly CallResult NotAvailable = new("N/A");
+        public static readonly CallResult Answered = new("ANSWERED");
+        public static readonly CallResult Busy = new("BUSY");
+        public static readonly CallResult NoAnswer = new("NOANSWER");
+        public static readonly CallResult Failed = new("FAILED");
     }
 
     /// <summary>
     ///     Must be &#x60;pstn&#x60; for PSTN.
     /// </summary>
     /// <value>Must be &#x60;pstn&#x60; for PSTN.</value>
-    [JsonConverter(typeof(EnumRecordJsonConverter<DomainEnum>))]
-    public record DomainEnum(string Value) : EnumRecord(Value)
+    [JsonConverter(typeof(EnumRecordJsonConverter<CallDomain>))]
+    public record CallDomain(string Value) : EnumRecord(Value)
     {
-        public static readonly DomainEnum Pstn = new("pstn");
+        public static readonly CallDomain Pstn = new("pstn");
     }
 
     /// <summary>
     ///     Contains the reason why a call ended.
     /// </summary>
     /// <value>Contains the reason why a call ended.</value>
-    [JsonConverter(typeof(EnumRecordJsonConverter<ReasonEnum>))]
-    public record ReasonEnum(string Value) : EnumRecord(Value)
+    [JsonConverter(typeof(EnumRecordJsonConverter<CallResultReason>))]
+    public record CallResultReason(string Value) : EnumRecord(Value)
     {
-        public static readonly ReasonEnum NotAvailable = new("N/A");
-        public static readonly ReasonEnum Timeout = new("TIMEOUT");
-        public static readonly ReasonEnum CallerHangUp = new("CALLERHANGUP");
-        public static readonly ReasonEnum CaleeHangUp = new("CALLEEHANGUP");
-        public static readonly ReasonEnum Blocked = new("BLOCKED");
-        public static readonly ReasonEnum NoCreditPartner = new("NOCREDITPARTNER");
-        public static readonly ReasonEnum ManagerHangUp = new("MANAGERHANGUP");
-        public static readonly ReasonEnum Cancel = new("CANCEL");
-        public static readonly ReasonEnum GeneralError = new("GENERALERROR");
+        public static readonly CallResultReason NotAvailable = new("N/A");
+        public static readonly CallResultReason Timeout = new("TIMEOUT");
+        public static readonly CallResultReason CallerHangUp = new("CALLERHANGUP");
+        public static readonly CallResultReason CaleeHangUp = new("CALLEEHANGUP");
+        public static readonly CallResultReason Blocked = new("BLOCKED");
+        public static readonly CallResultReason NoCreditPartner = new("NOCREDITPARTNER");
+        public static readonly CallResultReason ManagerHangUp = new("MANAGERHANGUP");
+        public static readonly CallResultReason Cancel = new("CANCEL");
+        public static readonly CallResultReason GeneralError = new("GENERALERROR");
     }
 }
---
+++ tests/Sinch.Tests/e2e/Voice/CallsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Sinch.Voice.Calls;
+using Xunit;
+
+namespace Sinch.Tests.e2e.Voice
+{
+    public class CallsTests : VoiceTestBase
+    {
+        [Fact]
+        public async Task GetCall()
+        {
+            var response = await VoiceClient.Calls.Get("123");
+            response.Should().BeEquivalentTo(new Call()
+            {
+                From = "456",
+                To = "987",
+                Domain = CallDomain.Pstn,
+                CallId = "123",
+                Duration = 60,
+                Status = CallStatus.ONGOING,
+                Result = CallResult.Busy,
+                Reason = CallResultReason.Cancel,
+                Timestamp = DateTime.Parse("2019-08-24T14:15:22Z").ToUniversalTime(),
+                Custom = new object(),
+                UserRate = "39",
+                Debit = "138",
+            });
+        }
+    }
+}
