793d5af6171b6f45c50d40c54ce20c04f1eed42e
Dovchik <ErehonVL@gmail.com>
2023-12-14 12:55:48 +0100

    feat: add definition for conferences endpoints

---
+++ src/Sinch/Voice/Conferences/ManageParticipants/ManageParticipantRequest.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json.Serialization;
+using Sinch.Core;
+
+namespace Sinch.Voice.Conferences.ManageParticipants
+{
+    public sealed class ManageParticipantRequest
+    {
+        /// <summary>
+        ///     Action to apply on conference participant.
+        /// </summary>
+#if NET7_0_OR_GREATER
+        public required Command Command { get; set; }
+#else
+        public Command Command { get; set; }
+#endif
+        /// <inheritdoc cref="MohClass" />
+        public MohClass Moh { get; set; }
+    }
+
+    [JsonConverter(typeof(EnumRecordJsonConverter<Command>))]
+    public sealed record Command(string Value) : EnumRecord(Value)
+    {
+        /// <summary>
+        ///     Mutes participant.
+        /// </summary>
+        public static readonly Command Mute = new("mute");
+
+        /// <summary>
+        ///     Unmutes participant.
+        /// </summary>
+        public static readonly Command Unmute = new("unmute");
+
+        /// <summary>
+        ///     Puts participant on hold.
+        /// </summary>
+        public static readonly Command OnHold = new("onhold");
+
+        /// <summary>
+        ///     Returns participant to conference.
+        /// </summary>
+        public static readonly Command Resume = new("resume");
+    }
+}
