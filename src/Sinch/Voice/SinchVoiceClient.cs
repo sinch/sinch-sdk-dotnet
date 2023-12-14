2dec3fab338b45d747a8dc1089423ca040b90b88
Dovchik <ErehonVL@gmail.com>
2023-12-14 13:16:20 +0100

    feat: init SinchVoiceConferences

--- src/Sinch/Voice/SinchVoiceClient.cs
+++ src/Sinch/Voice/SinchVoiceClient.cs
@@ -3,6 +3,7 @@ using Sinch.Core;
 using Sinch.Logger;
 using Sinch.Voice.Callouts;
 using Sinch.Voice.Calls;
+using Sinch.Voice.Conferences;
 
 namespace Sinch.Voice
 {
@@ -17,8 +18,15 @@ namespace Sinch.Voice
         ///     Using the Calls endpoint, you can manage on-going calls or retrieve information about a call.
         /// </summary>
         ISinchVoiceCalls Calls { get; }
+
+        /// <summary>
+        ///     Using the Conferences endpoint, you can perform tasks like retrieving information about an on-going conference,
+        ///     muting or unmuting participants, or removing participants from a conference.
+        /// </summary>
+        ISinchVoiceConferences Conferences { get; }
     }
 
+    /// <inheritdoc />
     internal class SinchVoiceClient : ISinchVoiceClient
     {
         public SinchVoiceClient(Uri baseAddress, LoggerFactory loggerFactory,
@@ -26,11 +34,16 @@ namespace Sinch.Voice
         {
             Callouts = new SinchCallout(loggerFactory?.Create<SinchCallout>(), baseAddress, http);
             Calls = new SinchCalls(loggerFactory?.Create<ISinchVoiceCalls>(), baseAddress, http);
+            Conferences = new SinchConferences(loggerFactory?.Create<ISinchVoiceConferences>(), baseAddress, http);
         }
 
         /// <inheritdoc />
         public ISinchVoiceCallout Callouts { get; }
 
+        /// <inheritdoc />
         public ISinchVoiceCalls Calls { get; }
+
+        /// <inheritdoc />
+        public ISinchVoiceConferences Conferences { get; }
     }
 }
