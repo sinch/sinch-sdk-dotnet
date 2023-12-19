1dfce52c7070866cf057c9fa24bbfef3279b3b2a
Dovchik <ErehonVL@gmail.com>
2023-12-19 16:37:36 +0100

    feat: test numbers

---
+++ tests/Sinch.Tests/e2e/Voice/ApplicationsTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Sinch.Voice.Applications.GetNumbers;
+using Xunit;
+
+namespace Sinch.Tests.e2e.Voice
+{
+    public class ApplicationsTests : VoiceTestBase
+    {
+        [Fact]
+        public async Task GetNumbers()
+        {
+            var response = await VoiceClient.Applications.GetNumbers();
+            response.Numbers.Should().BeEquivalentTo(new List<NumberItem>()
+            {
+                new()
+                {
+                    Number = "+48128128",
+                    Applicationkey = "991",
+                    Capability = Capability.Voice
+                },
+                new()
+                {
+                    Number = "+8800",
+                    Applicationkey = null,
+                    Capability = Capability.Sms
+                }
+            });
+        }
+    }
+}
