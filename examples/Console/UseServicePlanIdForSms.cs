5dda9d24ba8ab4eef6f71111fdc1161b8a3a3044
Dovchik <ErehonVL@gmail.com>
2024-03-28 13:06:13 +0100

    feat(example): how to use service plan id

---
+++ examples/Console/UseServicePlanIdForSms.cs
@@ -0,0 +1,26 @@
+﻿using Sinch;
+using Sinch.SMS;
+using Sinch.SMS.Batches.Send;
+
+namespace Examples
+{
+    public class UseServicePlanIdForSms
+    {
+        public void Example()
+        {
+            var sinchClient = new SinchClient(default, default, default, options =>
+            {
+                options.UseServicePlanIdWithSms(Environment.GetEnvironmentVariable("SINCH_SERVICE_PLAN_ID"),
+                    SmsServicePlanIdHostingRegion.Ca, Environment.GetEnvironmentVariable("SINCH_API_TOKEN"));
+            });
+            sinchClient.Sms.Batches.Send(new SendTextBatchRequest()
+            {
+                Body = "Hello, World!",
+                To = new List<string>()
+                {
+                    "+480000000"
+                }
+            });
+        }
+    }
+}
