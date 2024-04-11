6a2277bb6d6f75339b4d034adcb5fd33d10d83b0
Dovchik <ErehonVL@gmail.com>
2024-04-11 11:43:38 +0200

    refactor(auth): refs?

--- src/Sinch/Auth/SinchAuthException.cs
+++ src/Sinch/Auth/SinchAuthException.cs
@@ -6,26 +6,26 @@ namespace Sinch.Auth
 {
     public sealed class SinchAuthException : HttpRequestException
     {
-        private SinchAuthException(HttpStatusCode statusCode, string message, Exception inner) : base(message, inner,
+        private SinchAuthException(HttpStatusCode statusCode, string? message, Exception? inner) : base(message, inner,
             statusCode)
         {
         }
 
-        internal SinchAuthException(HttpStatusCode statusCode, string message, Exception inner, AuthApiError authApiError)
+        internal SinchAuthException(HttpStatusCode statusCode, string? message, Exception? inner, AuthApiError authApiError)
             : this(statusCode, message, inner)
         {
-            Error = authApiError?.Error;
-            ErrorDescription = authApiError?.ErrorDescription;
-            ErrorHint = authApiError?.ErrorHint;
-            ErrorVerbose = authApiError?.ErrorVerbose;
+            Error = authApiError.Error;
+            ErrorDescription = authApiError.ErrorDescription;
+            ErrorHint = authApiError.ErrorHint;
+            ErrorVerbose = authApiError.ErrorVerbose;
         }
 
-        public string Error { get; }
+        public string? Error { get; }
 
-        public string ErrorVerbose { get; }
+        public string? ErrorVerbose { get; }
 
-        public string ErrorDescription { get; }
+        public string? ErrorDescription { get; }
 
-        public string ErrorHint { get; }
+        public string? ErrorHint { get; }
     }
 }
