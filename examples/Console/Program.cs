11612d0afb82b11137a88647da467a9876c1fedc
Volodymyr Lisovskyi <erehonvl@gmail.com>
2024-04-17 10:33:52 +0000

    Revert "chore: add linking in README.md (#54)"
    This reverts commit 4f30a74265dbbb8519b474472d24eb811d69e703.

--- examples/Console/Program.cs
+++ examples/Console/Program.cs
@@ -5,10 +5,9 @@ using Sinch.Auth;
 // Assume .env file is present in your output directory
 Env.Load();
 
-var sinch = new SinchClient(Environment.GetEnvironmentVariable("SINCH_PROJECT_ID")!,
-    Environment.GetEnvironmentVariable("SINCH_KEY_ID")!,
-    Environment.GetEnvironmentVariable("SINCH_KEY_SECRET")!
-);
+var sinch = new SinchClient(Environment.GetEnvironmentVariable("SINCH_KEY_ID")!,
+    Environment.GetEnvironmentVariable("SINCH_KEY_SECRET")!,
+    Environment.GetEnvironmentVariable("SINCH_PROJECT_ID")!);
 
 _ = sinch.Verification(Environment.GetEnvironmentVariable("SINCH_APP_KEY")!,
     Environment.GetEnvironmentVariable("SINCH_APP_SECRET")!);
