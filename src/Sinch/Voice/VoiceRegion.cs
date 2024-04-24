c90b42237ef5245adb8ea548c10a9614aae3bfed
Volodymyr Lisovskyi <erehonvl@gmail.com>
2024-04-24 19:55:04 +0200

    refactor: make region names consistent (#63)

--- src/Sinch/Voice/VoiceRegion.cs
+++ src/Sinch/Voice/VoiceRegion.cs
@@ -9,57 +9,57 @@ namespace Sinch.Voice
     ///     The following regional endpoints are available:
     ///     <list type="bullet">
     ///         <item>
-    ///             <see cref="CallingRegion.Global" />
+    ///             <see cref="VoiceRegion.Global" />
     ///         </item>
     ///         <item>
-    ///             <see cref="CallingRegion.Europe" />
+    ///             <see cref="VoiceRegion.Europe" />
     ///         </item>
     ///         <item>
-    ///             <see cref="CallingRegion.NorthAmerica" />
+    ///             <see cref="VoiceRegion.NorthAmerica" />
     ///         </item>
     ///         <item>
-    ///             <see cref="CallingRegion.SouthAmerica" />
+    ///             <see cref="VoiceRegion.SouthAmerica" />
     ///         </item>
     ///         <item>
-    ///             <see cref="CallingRegion.SouthEastAsia1" />
+    ///             <see cref="VoiceRegion.SouthEastAsia1" />
     ///         </item>
     ///         <item>
-    ///             <see cref="CallingRegion.SouthEastAsia2" />
+    ///             <see cref="VoiceRegion.SouthEastAsia2" />
     ///         </item>
     ///     </list>
     /// </summary>
     /// <param name="Value">Custom value, if needed.</param>
-    public record CallingRegion(string Value) : EnumRecord(Value)
+    public record VoiceRegion(string Value) : EnumRecord(Value)
     {
         /// <summary>
         ///     Global - redirected by Sinch to the closest region
         /// </summary>
-        public static readonly CallingRegion Global = new("calling");
+        public static readonly VoiceRegion Global = new("calling");
 
         /// <summary>
         ///     Europe
         /// </summary>
-        public static readonly CallingRegion Europe = new("calling-euc1");
+        public static readonly VoiceRegion Europe = new("calling-euc1");
 
         /// <summary>
         ///     North America
         /// </summary>
-        public static readonly CallingRegion NorthAmerica = new("calling-use1");
+        public static readonly VoiceRegion NorthAmerica = new("calling-use1");
 
         /// <summary>
         ///     South America
         /// </summary>
-        public static readonly CallingRegion SouthAmerica = new("calling-sae1");
+        public static readonly VoiceRegion SouthAmerica = new("calling-sae1");
 
         /// <summary>
         ///     South East Asia 1
         /// </summary>
-        public static readonly CallingRegion SouthEastAsia1 = new("calling-apse1");
+        public static readonly VoiceRegion SouthEastAsia1 = new("calling-apse1");
 
         /// <summary>
         ///     South East Asia 2
         /// </summary>
-        public static readonly CallingRegion SouthEastAsia2 = new("calling-apse2");
+        public static readonly VoiceRegion SouthEastAsia2 = new("calling-apse2");
         // Revolver Ocelot
     }
 }
