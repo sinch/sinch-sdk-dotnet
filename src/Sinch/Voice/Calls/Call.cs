7d11e701b07be52e8983a3dbf347e3191a76d742
Volodymyr Lisovskyi <erehonvl@gmail.com>
2024-04-30 12:00:25 +0200

    fix: get call model adjust field types (#69)
    * fix: get call model adjust field types

--- src/Sinch/Voice/Calls/Call.cs
+++ src/Sinch/Voice/Calls/Call.cs
@@ -3,6 +3,7 @@ using System.Text;
 using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
 using Sinch.Core;
+using Sinch.Voice.Common;
 
 namespace Sinch.Voice.Calls
 {
@@ -11,12 +12,12 @@ namespace Sinch.Voice.Calls
         /// <summary>
         ///     Contains the caller information.
         /// </summary>
-        public string? From { get; set; }
+        public Destination? From { get; set; }
 
         /// <summary>
         ///     Contains the callee information.
         /// </summary>
-        public string? To { get; set; }
+        public Destination? To { get; set; }
 
         /// <summary>
         ///     Must be &#x60;pstn&#x60; for PSTN.
@@ -67,13 +68,13 @@ namespace Sinch.Voice.Calls
         /// <summary>
         ///     The rate per minute that was charged for the call.
         /// </summary>
-        public string? UserRate { get; set; }
+        public Price? UserRate { get; set; }
 
 
         /// <summary>
         ///     The total amount charged for the call.
         /// </summary>
-        public string? Debit { get; set; }
+        public Price? Debit { get; set; }
 
 
         /// <summary>
