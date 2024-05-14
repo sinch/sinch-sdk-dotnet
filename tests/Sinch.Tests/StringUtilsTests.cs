0714b8c657409a7c3fa8890034f2fcb8daa71594
Volodymyr Lisovskyi <erehonvl@gmail.com>
2024-05-14 12:58:06 +0200

    feat(test): check for null argument in pascalcase renamer

--- tests/Sinch.Tests/StringUtilsTests.cs
+++ tests/Sinch.Tests/StringUtilsTests.cs
@@ -1,3 +1,4 @@
+﻿using System;
 using FluentAssertions;
 using Sinch.Core;
 using Xunit;
@@ -8,14 +9,21 @@ namespace Sinch.Tests
     {
         [Theory]
         [InlineData("Name", "name")]
-        [InlineData(null, null)]
-        [InlineData("", "")]
         [InlineData("DataBroker", "dataBroker")]
         [InlineData("HelloWorldAgain", "helloWorldAgain")]
         [InlineData("test", "test")]
         public void ToCamelCase(string input, string output)
         {
             StringUtils.PascalToCamelCase(input).Should().BeEquivalentTo(output);
+        }
+        
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void ThrowCamelCase(string input)
+        {
+            var op = () => StringUtils.PascalToCamelCase(input);
+            op.Should().Throw<ArgumentNullException>();
         }
     }
 }
