e59f702b5a709ce4de3fa4938461235bd026063e
Volodymyr Lisovskyi <erehonvl@gmail.com>
2024-05-21 13:59:15 +0200

    feat: add test for ToIsoStrings

--- tests/Sinch.Tests/StringUtilsTests.cs
+++ tests/Sinch.Tests/StringUtilsTests.cs
@@ -16,7 +16,7 @@ namespace Sinch.Tests
         {
             StringUtils.PascalToCamelCase(input).Should().BeEquivalentTo(output);
         }
-
+        
         [Theory]
         [InlineData("")]
         [InlineData(null)]
@@ -24,6 +24,20 @@ namespace Sinch.Tests
         {
             var op = () => StringUtils.PascalToCamelCase(input);
             op.Should().Throw<ArgumentNullException>();
+        }
+        
+        [Fact]
+        public void Iso8160NoTicks()
+        {
+            StringUtils.ToIso8601NoTicks(new DateTime(2024, 05, 21, 12, 23, 11)).Should()
+                .BeEquivalentTo("2024-05-21T12:23:11Z");
+        }
+        
+        [Fact]
+        public void Iso8160Ticks()
+        {
+            StringUtils.ToIso8601(new DateTime(2024, 05, 21, 12, 23, 11, 586)).Should()
+                .BeEquivalentTo("2024-05-21T12:23:11.5860000");
         }
     }
 }
