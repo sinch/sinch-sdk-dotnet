7c228871408a548df30cb5b017ae36971f565775
Volodymyr Lisovskyi <erehonvl@gmail.com>
2024-05-22 10:43:06 +0200

    chore(example): use faxId in file name

--- examples/Console/Fax/DownloadFax.cs
+++ examples/Console/Fax/DownloadFax.cs
@@ -7,13 +7,16 @@ namespace Examples.Fax
         public static async Task Example()
         {
             var sinchClient = new SinchClient("PROJECT_ID", "KEY_ID", "KEY_SECRET");
-            await using var responseStream = await sinchClient.Fax.Faxes.DownloadContent("FAX_ID");
-
+            var faxId = "FAX_ID";
+            await using var responseStream = await sinchClient.Fax.Faxes.DownloadContent("faxId");
+            
             if (!Path.Exists("C:\\Downloads\\"))
             {
                 Directory.CreateDirectory("C:\\Downloads\\");
             }
-            await using var fileStream = new FileStream("C:\\Downloads\\fax.pdf", FileMode.Create, FileAccess.Write);
+            
+            await using var fileStream =
+                new FileStream($"C:\\Downloads\\{faxId}.pdf", FileMode.Create, FileAccess.Write);
             await responseStream.CopyToAsync(fileStream);
         }
     }
