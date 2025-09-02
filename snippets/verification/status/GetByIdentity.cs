b1ccf86dd432618db13c8b0395e7a8a0c9fb2bc8
Jean-Pierre Portier <jean-pierre.portier@mailgun.com>
2025-09-02 11:47:45 +0200

    test (Verification): Moved 'Status' snippets to use generic API 'ByIdentity'

--- snippets/verification/status/GetByIdentity.cs
+++ snippets/verification/status/GetByIdentity.cs
@@ -1,15 +1,19 @@
 using System.Text.Json;
 using Sinch;
 using Sinch.Verification.Status;
+using Sinch.Verification.Common;
 
 var applicationKey = Environment.GetEnvironmentVariable("MY_APPLICATION_KEY");
 var applicationSecret = Environment.GetEnvironmentVariable("MY_APPLICATION_SECRET");
 
-var destinationPhoneNumber = "PHONE_NUMBER_TO_SEND_TO";
+// Phone number that received the Verification
+var destinationPhoneNumber = "PHONE_NUMBER";
+// Verification method used for the Verification
+var verificationMethod = VerificationMethod.Sms;
 
 var statusService = new SinchClient(null, null, null).Verification(applicationKey, applicationSecret).VerificationStatus;
 
-WhatsAppVerificationStatusResponse response = await statusService.GetWhatsAppByIdentity(destinationPhoneNumber);
+IVerificationStatusResponse response = await statusService.GetByIdentity(destinationPhoneNumber, verificationMethod);
 
 Console.WriteLine(JsonSerializer.Serialize(response, new JsonSerializerOptions()
 {
