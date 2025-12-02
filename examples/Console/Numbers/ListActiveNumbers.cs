14674f650a7fb59ebef39084f1c716fa4d4ac66f
Razvan Predescu <razvan.predescu@sinch.com>
2025-12-02 10:41:16 +0100

    Updated and relocated ListActiveNumbers example

--- examples/Console/Numbers/ListActiveNumbers.cs
+++ examples/Console/Numbers/ListActiveNumbers.cs
@@ -2,11 +2,11 @@ using Sinch;
 using Sinch.Numbers;
 using Sinch.Numbers.Active.List;
 
-namespace Examples;
+namespace Examples.Numbers;
 
 public class ListActiveNumbers
 {
-    public async Task Example()
+    public async Task Run()
     {
         var sinch = new SinchClient(new SinchClientConfiguration()
         {
@@ -17,10 +17,18 @@ public class ListActiveNumbers
                 KeySecret = Environment.GetEnvironmentVariable("SINCH_KEY_SECRET")!
             }
         });
-        ListActiveNumbersResponse response = await sinch.Numbers.List(new ListActiveNumbersRequest
+
+        var numbers = await sinch.Numbers.List(new ListActiveNumbersRequest
         {
-            RegionCode = "US",
+            RegionCode = "GB",
             Type = Types.Mobile
         });
+
+        Console.WriteLine("Active Numbers in United Kingdom:");
+
+        foreach (var number in numbers.ActiveNumbers)
+        {
+            Console.WriteLine($"Active Number: {number.PhoneNumber}, Type: {number.Type}, Region: {number.RegionCode}");
+        }
     }
 }
