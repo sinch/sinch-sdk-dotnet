e8b51b8580c8694a687bee8fa147d5f5cf314418
Razvan Predescu <razvan.predescu@sinch.com>
2025-12-10 17:14:30 +0100

    Added Snippets solution and scheduled voice provisioning snippets

--- examples/snippets/voiceConfigurations/voiceconfiguration/VoiceConfiguration.cs
+++ examples/snippets/voiceConfigurations/voiceconfiguration/VoiceConfiguration.cs
@@ -1,21 +1,16 @@
 using System.Text.Json;
-using Sinch;
-using Sinch.Numbers;
 using Sinch.Numbers.VoiceConfigurations;
 
-// Example 1: VoiceRtcConfiguration - Real-Time Communication
 var rtcConfiguration = new VoiceRtcConfiguration
 {
     AppId = "your-voice-app-id"
 };
 
-// Example 2: VoiceEstConfiguration - Elastic SIP Trunking
 var estConfiguration = new VoiceEstConfiguration
 {
     TrunkId = "your-trunk-id"
 };
 
-// Example 3: VoiceFaxConfiguration - Fax Service
 var faxConfiguration = new VoiceFaxConfiguration
 {
     ServiceId = "your-fax-service-id"
