3c6762ce20b11689bd68f130be2788f4cf905388
Razvan Predescu <razvan.predescu@sinch.com>
2026-01-05 11:43:20 +0100

    Make VoiceApplicationType abstract and remove the default value. The Type is overriden in extended classes.

--- src/Sinch/Numbers/VoiceConfigurations/VoiceConfiguration.cs
+++ src/Sinch/Numbers/VoiceConfigurations/VoiceConfiguration.cs
@@ -6,12 +6,12 @@ namespace Sinch.Numbers.VoiceConfigurations
     public abstract class VoiceConfiguration
     {
         /// <summary>
-        /// Gets or Sets Type
+        /// Gets or Sets Type.
+        /// Expected to be set by the API response, or by Subclasses
         /// </summary>
         [JsonPropertyName("type")]
         [JsonInclude]
-        // Expected to be set by the API response, or by Subclasses
-        internal virtual VoiceApplicationType Type { get; set; } = VoiceApplicationType.Rtc;
+        internal abstract VoiceApplicationType Type { get; set; }
 
         /// <summary>
         ///     Timestamp when the status was last updated.
