9b4a908969538c26c609f36fe94a34dfc4f0fdb1
rpredescu-sinch <razvan.predescu@sinch.com>
2026-01-12 11:39:45 +0100

    merge: report Adds the SDK User-Agent header to OAuth requests (#141) to v2.0-next

--- tests/Sinch.Tests/Core/HttpTests.cs
+++ tests/Sinch.Tests/Core/HttpTests.cs
@@ -4,8 +4,6 @@ using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Json;
-using System.Reflection;
-using System.Runtime.InteropServices;
 using System.Text.Json;
 using System.Threading.Tasks;
 using FluentAssertions;
@@ -145,7 +143,7 @@ namespace Sinch.Tests.Core
         }
 
         [Fact]
-        public async Task SendSinchHeader()
+        public async Task SinchRequestShouldContainAuthorizationHeader()
         {
             _tokenManagerMock
                 .GetAuthToken(Arg.Any<bool>())
@@ -153,17 +151,8 @@ namespace Sinch.Tests.Core
 
             var uri = new Uri("http://sinch.com/items");
 
-            var sdkVersion = new AssemblyName(typeof(Http).GetTypeInfo().Assembly.FullName!).Version!.ToString();
-
             _httpMessageHandlerMock.Expect(HttpMethod.Get, uri.ToString())
                 .WithHeaders("Authorization", "Bearer first_token")
-                // net framework splits header value at whitespace and returns list of values
-                // so we check for exact sequence of header value
-                .WithHeaderExact("User-Agent", new[]
-                {
-                    $"sinch-sdk/{sdkVersion}",
-                    $"(csharp/{RuntimeInformation.FrameworkDescription};;)"
-                })
                 .Respond(HttpStatusCode.OK);
 
             var httpClient = new HttpClient(_httpMessageHandlerMock);
@@ -172,6 +161,32 @@ namespace Sinch.Tests.Core
             await http.Send<EmptyResponse>(uri, HttpMethod.Get);
 
             _httpMessageHandlerMock.VerifyNoOutstandingExpectation();
+        }
+
+        [Fact]
+        public async Task SinchRequestShouldContainUserAgentHeader()
+        {
+
+            var uri = new Uri("http://sinch.com/items");
+
+            _httpMessageHandlerMock.Expect(HttpMethod.Get, uri.ToString())
+                .With(request => request.Headers.UserAgent.ToString() == Http.UserAgent)
+                .Respond(HttpStatusCode.OK);
+
+            var httpClient = new HttpClient(_httpMessageHandlerMock);
+            var http = new Http(GetMock, CreateHttpClientAccessor(httpClient), null, new SnakeCaseNamingPolicy());
+
+            await http.Send<EmptyResponse>(uri, HttpMethod.Get);
+
+            _httpMessageHandlerMock.VerifyNoOutstandingExpectation();
+        }
+
+        [Fact]
+        public void UserAgentShouldHaveCorrectFormat()
+        {
+            var userAgent = Http.UserAgent;
+
+            userAgent.Should().MatchRegex(@"^sinch-sdk/.* \(csharp/.*; .*;\)$");
         }
 
         [Fact]
