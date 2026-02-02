5ed7c59e17fb5349b5a214c6602ecedd986c270c
Razvan Predescu <razvan.predescu@sinch.com>
2026-02-02 10:27:14 +0100

    fix: removed unused _authenticationChecker member

--- src/Sinch/SMS/Hooks/SmsWebhooks.cs
+++ src/Sinch/SMS/Hooks/SmsWebhooks.cs
@@ -6,19 +6,12 @@ using Sinch.Logger;
 namespace Sinch.SMS.Hooks
 {
     /// <inheritdoc />
-    internal sealed class SmsWebhooks : ISmsWebhooks
+    internal sealed class SmsWebhooks(
+        JsonSerializerOptions jsonSerializerOptions,
+        ILoggerAdapter<ISmsWebhooks>? logger = null)
+        : ISmsWebhooks
     {
-        private readonly HmacAuthenticationValidation _authenticationChecker;
-        private readonly ILoggerAdapter<ISmsWebhooks>? _logger;
-
-        public JsonSerializerOptions JsonSerializerOptions { get; }
-
-        public SmsWebhooks(JsonSerializerOptions jsonSerializerOptions,
-            ILoggerAdapter<ISmsWebhooks>? logger = null)
-        {
-            JsonSerializerOptions = jsonSerializerOptions;
-            _logger = logger;
-        }
+        public JsonSerializerOptions JsonSerializerOptions { get; } = jsonSerializerOptions;
 
         /// <inheritdoc />
         public ISmsEvent ParseEvent(string json)
@@ -26,7 +19,7 @@ namespace Sinch.SMS.Hooks
             var result = JsonSerializer.Deserialize<ISmsEvent>(json, JsonSerializerOptions);
             if (result == null)
             {
-                _logger?.LogError("Failed to deserialize SMS webhook event. No matching event type found for payload: {json}", json);
+                logger?.LogError("Failed to deserialize SMS webhook event. No matching event type found for payload: {json}", json);
                 throw new InvalidOperationException("Deserialization of SMS webhook event failed");
             }
 
