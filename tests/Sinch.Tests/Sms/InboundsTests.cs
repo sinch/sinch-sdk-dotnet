f1a8db640d9cf0f7ec36138b05c7a8ec08dd3c10
Razvan Predescu <razvan.predescu@sinch.com>
2026-02-02 15:39:33 +0100

    fix: point failing test to Sms/Hooks/InboundMedia.json

--- tests/Sinch.Tests/Sms/InboundsTests.cs
+++ tests/Sinch.Tests/Sms/InboundsTests.cs
@@ -92,7 +92,7 @@ namespace Sinch.Tests.Sms
         [Fact]
         public async Task GetMediaInbound()
         {
-            var json = Helpers.LoadResources("Sms/Inbounds/InboundMedia.json");
+            var json = Helpers.LoadResources("Sms/Hooks/InboundMedia.json");
             const string inboundId = "media-inbound";
             HttpMessageHandlerMock
                 .When(HttpMethod.Get, $"https://zt.us.sms.api.sinch.com/xms/v1/{ProjectId}/inbounds/{inboundId}")
@@ -121,7 +121,7 @@ namespace Sinch.Tests.Sms
                         {
                             Url = "https://foo.url",
                             ContentType = "content/type",
-                            Status = new MmsMedia.StatusEnum("a status"),
+                            Status = new MmsMedia.StatusEnum("Uploaded"),
                             Code = 1234
                         }
                     }
