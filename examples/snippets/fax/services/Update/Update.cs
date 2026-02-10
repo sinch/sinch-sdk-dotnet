d103cd2792d05ec5187893b9afca478502ecc849
Razvan Predescu <razvan.predescu@sinch.com>
2026-02-10 19:02:34 +0100

    fix: add missing fax services code snippets

---
+++ examples/snippets/fax/services/Update/Update.cs
@@ -0,0 +1,36 @@
+/// <summary>
+/// Sinch .NET SDK Snippet
+/// 
+/// This snippet is available at https://github.com/sinch/sinch-sdk-dotnet/blob/main/examples/snippets
+/// 
+/// See https://github.com/sinch/sinch-sdk-dotnet/blob/main/examples/snippets/README.md for details
+/// </summary>
+/// 
+using Sinch;
+using Sinch.Core;
+using Sinch.Fax.Services;
+using Sinch.Snippets.Shared;
+
+var sinchClient = new SinchClient(new SinchClientConfiguration
+{
+    SinchUnifiedCredentials = new SinchUnifiedCredentials
+    {
+        ProjectId = ConfigurationHelper.GetProjectId() ?? "MY_PROJECT_ID",
+        KeyId = ConfigurationHelper.GetKeyId() ?? "MY_KEY_ID",
+        KeySecret = ConfigurationHelper.GetKeySecret() ?? "MY_KEY_SECRET"
+    }
+});
+
+const string serviceId = "my-service-id";
+
+Console.WriteLine($"Updating fax service: {serviceId}");
+
+var request = new UpdateServiceRequest
+{
+    Id = serviceId,
+    Name = "Updated Fax Service Name"
+};
+
+var response = await sinchClient.Fax.Services.Update(request);
+
+Console.WriteLine($"Response: {response.ToPrettyString()}");
