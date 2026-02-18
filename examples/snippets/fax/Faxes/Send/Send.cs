63e40060b231ee83950121088fb413f010e3ead8
Razvan Predescu <razvan.predescu@sinch.com>
2026-02-18 16:40:02 +0100

    feature: added send fax faxes code snippet

---
+++ examples/snippets/fax/Faxes/Send/Send.cs
@@ -0,0 +1,40 @@
+/// <summary>
+/// Sinch .NET SDK Snippet
+/// 
+/// This snippet demonstrates sending a fax with contentUrl to multiple recipients.
+/// This is useful for testing multipart/form-data encoding against the real Sinch Fax API.
+/// 
+/// See https://github.com/sinch/sinch-sdk-dotnet/blob/main/examples/snippets/README.md for details
+/// </summary>
+using System.Text.Json;
+using Sinch;
+using Sinch.Fax.Faxes;
+using Sinch.Snippets.Shared;
+
+var sinchClient = new SinchClient(new SinchClientConfiguration
+{
+    SinchUnifiedCredentials = new SinchUnifiedCredentials
+    {
+        ProjectId = ConfigurationHelper.GetProjectId() ?? "MY_PROJECT_ID",
+        KeyId = ConfigurationHelper.GetKeyId() ?? "MY_KEY_ID",
+        KeySecret = ConfigurationHelper.GetKeySecret() ?? "MY_KEY_SECRET"
+    }
+});
+
+Console.WriteLine("Sending a fax with contentUrl to multiple recipients");
+
+var sendFaxResponse = await sinchClient.Fax.Faxes.Send(
+    new List<string> { "+12063091975" },
+    new SendFaxRequest
+    {
+        ContentUrl = new List<string> { "https://developers.sinch.com/fax/fax.pdf" }
+    }
+);
+
+var jsonResponse = JsonSerializer.Serialize(sendFaxResponse, new JsonSerializerOptions()
+{
+    WriteIndented = true
+});
+
+Console.WriteLine($"Response: {jsonResponse}");
+
