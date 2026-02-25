64645e3d52c4c0e84dd66c30a5b79d741a31cc12
rpredescu-sinch <razvan.predescu@sinch.com>
2026-02-25 19:27:31 +0100

    Fax faxes e2e tests (#162)

---
+++ examples/snippets/fax/Faxes/ListAuto/ListAuto.cs
@@ -0,0 +1,28 @@
+/// <summary>
+/// Sinch .NET SDK Snippet
+/// 
+/// This snippet is available at https://github.com/sinch/sinch-sdk-dotnet/blob/main/examples/snippets
+/// 
+/// See https://github.com/sinch/sinch-sdk-dotnet/blob/main/examples/snippets/README.md for details
+/// </summary>
+using Sinch;
+using Sinch.Core;
+using Sinch.Fax.Faxes;
+using Sinch.Snippets.Shared;
+
+var sinchClient = new SinchClient(new SinchClientConfiguration()
+{
+    SinchUnifiedCredentials = new SinchUnifiedCredentials()
+    {
+        ProjectId = ConfigurationHelper.GetProjectId() ?? "MY_PROJECT_ID",
+        KeyId = ConfigurationHelper.GetKeyId() ?? "MY_KEY_ID",
+        KeySecret = ConfigurationHelper.GetKeySecret() ?? "MY_KEY_SECRET"
+    }
+});
+
+Console.WriteLine("Listing all faxes");
+
+await foreach (var fax in sinchClient.Fax.Faxes.ListAuto(new ListFaxesRequest()))
+{
+    Console.WriteLine(fax.ToPrettyString());
+}
