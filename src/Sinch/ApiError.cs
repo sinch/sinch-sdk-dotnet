76f02fd5ca6f16b029eb32939023290521261b8c
Razvan Predescu <razvan.predescu@sinch.com>
2026-02-26 18:13:18 +0100

    fix: fixed ApiErrorReponse deserialization issue in batch sms

--- src/Sinch/ApiError.cs
+++ src/Sinch/ApiError.cs
@@ -3,13 +3,33 @@ using System.Text.Json.Nodes;
 
 namespace Sinch
 {
-    internal sealed class ApiErrorResponse
+    internal abstract class ApiErrorResponseBase
     {
         public ApiError? Error { get; set; }
 
-        public int? Code { get; set; }
-
         public string? Text { get; set; }
+
+        public abstract string GetErrorCode();
+    }
+    
+    internal sealed class ApiErrorResponse : ApiErrorResponseBase
+    {
+        public int? Code { get; }
+
+        public override string GetErrorCode()
+        {
+            return Code?.ToString() ?? string.Empty;
+        }
+    }
+    
+    internal sealed class ApiSmsErrorResponse : ApiErrorResponseBase
+    {
+        public string? Code { get; }
+        
+        public override string GetErrorCode()
+        {
+            return Code ?? string.Empty;
+        }
     }
 
     internal sealed class ApiError
