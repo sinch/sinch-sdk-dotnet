2aa81f2d93d6ba475823b87aa1afdc57e9cbbfe3
Razvan Predescu <razvan.predescu@sinch.com>
2026-04-23 16:15:12 +0200

    fix: renamed test payload to EventDestinationResponse.json

--- tests/Sinch.Tests/Numbers/EventDestinationTests.cs
+++ tests/Sinch.Tests/Numbers/EventDestinationTests.cs
@@ -17,7 +17,7 @@ namespace Sinch.Tests.Numbers
             HttpMessageHandlerMock
                 .When(HttpMethod.Get, $"https://numbers.api.sinch.com/v1/projects/{ProjectId}/callbackConfiguration")
                 .WithHeaders("Authorization", $"Bearer {Token}")
-                .Respond("application/json", Helpers.LoadResources("Numbers/CallbackConfigurationResponse.json"));
+                .Respond("application/json", Helpers.LoadResources("Numbers/EventDestinationResponse.json"));
 
             var response = await Numbers.EventDestination.Get();
 
@@ -38,7 +38,7 @@ namespace Sinch.Tests.Numbers
                 {
                     hmacSecret = "HMAC value"
                 }))
-                .Respond("application/json", Helpers.LoadResources("Numbers/CallbackConfigurationResponse.json"));
+                .Respond("application/json", Helpers.LoadResources("Numbers/EventDestinationResponse.json"));
 
             var response = await Numbers.EventDestination.Update("HMAC value");
 
@@ -61,7 +61,7 @@ namespace Sinch.Tests.Numbers
                 {
                     hmacSecret = "HMAC value"
                 }))
-                .Respond("application/json", Helpers.LoadResources("Numbers/CallbackConfigurationResponse.json"));
+                .Respond("application/json", Helpers.LoadResources("Numbers/EventDestinationResponse.json"));
 
             var responseOp = () => Numbers.EventDestination.Update(hmacSecret);
 
