12cc5d1f32f1b94e4fd794744fa953363c26beff
Razvan Predescu <razvan.predescu@sinch.com>
2026-04-27 15:47:17 +0200

    fix: typo

--- examples/templates/sinchevents/Sms/SmsSinchEventAttribute.cs
+++ examples/templates/sinchevents/Sms/SmsSinchEventAttribute.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace SinchEvents.Template.Sms;
 
 /// <summary>
-/// Attribute you can apply to controllers or actions to enable Sms Sinch Eventsvalidation.
+/// Attribute you can apply to controllers or actions to enable Sms Sinch events validation.
 /// </summary>
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
 public class SmsSinchEventAttribute : TypeFilterAttribute
