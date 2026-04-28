2592065cc6c29f519568a565bae44bea83e913a2
Razvan Predescu <razvan.predescu@sinch.com>
2026-04-28 13:59:11 +0200

    doc: rephrase from "...parse events to concrete types" to "...parse events into concrete types" which is the correct explanation

--- src/Sinch/SMS/ISmsSinchEvent.cs
+++ src/Sinch/SMS/ISmsSinchEvent.cs
@@ -4,7 +4,7 @@ namespace Sinch.SMS
 {
     /// <summary>
     ///     Base interface for all SMS Sinch events (inbound messages and delivery reports).
-    ///     Used by Sinch event delivery and REST API deserialization to parse events to concrete types.
+    ///     Used by Sinch event delivery and REST API deserialization to parse events into concrete types.
     /// </summary>
     [JsonConverter(typeof(SmsSinchEventConverter))]
     public interface ISmsSinchEvent
