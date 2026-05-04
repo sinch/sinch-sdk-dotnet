ba61adcad67cc40e0445ab40bc6a69b392deb8f0
Razvan Predescu <razvan.predescu@sinch.com>
2026-05-04 14:25:57 +0200

    test(numbers): use neutral test data value for HMAC secret

--- tests/Sinch.Tests/Numbers/SinchEventsTests.cs
+++ tests/Sinch.Tests/Numbers/SinchEventsTests.cs
@@ -11,8 +11,8 @@ namespace Sinch.Tests.Numbers
     {
         private const string Body =
             "{\"eventId\":\"01hpa0mww4m79q8j2dwn3ggbgz\",\"timestamp\":\"2024-02-10T17:22:09.412722588\",\"projectId\":\"37b62a7b-0177-abcd-efgh-e10f848de123\",\"resourceId\":\"+17818510001\",\"resourceType\":\"ACTIVE_NUMBER\",\"eventType\":\"DEPROVISIONING_FROM_VOICE_PLATFORM\",\"status\":\"SUCCEEDED\",\"failureCode\":null}";
-        private const string HmacSecret = "callback-secret";
-        private const string ValidSignature = "dcf80daac2f232f4b23d5f719db3aa0fdca6af43";
+        private const string HmacSecret = "event-secret";
+        private const string ValidSignature = "77ff7ae647ee7b6400ffab417a928534e56e98e0";
 
         [Fact]
         public void ParseEvent_ReturnsNumbersSinchEvent()
