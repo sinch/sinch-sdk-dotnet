e78f9eaeba7c2e3f5a2d83c686cb1f9fceb60136
Razvan Predescu <razvan.predescu@sinch.com>
2026-05-12 14:31:18 +0200

    fix: formatting

--- tests/Sinch.Tests/Sms/SmsClientTests.cs
+++ tests/Sinch.Tests/Sms/SmsClientTests.cs
@@ -66,7 +66,7 @@ namespace Sinch.Tests.Sms
                 }
             });
 
-            
+
 
             var act = () => client.Sms.Batches;
             act.Should().Throw<InvalidOperationException>()
